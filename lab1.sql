e0283ae9ee35bc8a682c2c4f9dc9ea2c3c005d9d
rosyk <rosyknikita@gmail.com>
2023-03-17 11:44:22 +0200

    lab 1

---
+++ lab1.sql
@@ -0,0 +1,21 @@
+SELECT name
+FROM cities
+ORDER BY population DESC
+LIMIT 10 OFFSET 10;
+
+SELECT name
+FROM cities
+ORDER BY name DESC
+LIMIT 30;
+
+SELECT name
+FROM cities
+ORDER BY region, population DESC;
+
+SELECT DISTINCT region
+FROM cities;
+
+SELECT name
+FROM cities
+ORDER BY region DESC, name DESC;
+
